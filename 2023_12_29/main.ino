4822076c4e82ffec9d2c18c97ce8bd905dbbfd6a
sora81dev <sora81dev@gmail.com>
2023-12-29 17:06:36 +0900

    20231229&20231223　追加

--- 2023_12_29/main.ino
+++ 2023_12_29/main.ino
@@ -53,19 +53,18 @@ void loop() {
       // コンパス
       imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-      if (A8 >= 250) {  //ラインに触れているとき
+      if (A8 >= 160) {  //ラインに触れているとき
         analogWrite(5, 0);
         analogWrite(6, 0);
         analogWrite(7, 125);
         analogWrite(8, 0);
         analogWrite(9, 0);
         analogWrite(10, 150);
-        delay(200);
       } else {
 
         // 姿勢制御
         if (360 - Error_margin <= (int)floor(euler.x()) || (int)floor(euler.x()) <= 0 + Error_margin) {  // ロボットが中心地からずれていないとき
-        /*          
+                                                                                                         /*          
                   A2 
           A3 --------------  A5
              |            |
@@ -77,21 +76,23 @@ void loop() {
                   A1
 
         */
-          if ((A2 <= A4 && A2 <= A7 && A2 <= A1) && A2 <= 500) {  //前が最も大きい
+          if ((A2 <= A4 && A2 <= A7 && A2 <= A1) && A2 <= 800) {                                         //前が最も大きい
             analogWrite(5, 0);
             analogWrite(6, 0);
             analogWrite(7, 0);
             analogWrite(8, 125);
             analogWrite(9, 150);
             analogWrite(10, 0);
-          } else if (((A7 <= A2 || A7 <= A1) || (A4 <= A2 || A4 <= A1)) && (A7 >= 500) || (A4 >= 500)) {  //ボールがロボットの右or左にある
+          } /*else if (((A7 <= A2 || A7 <= A1) || (A4 <= A2 || A4 <= A1)) && (A7 >= 500 || A4 >= 500)) {  //ボールがロボットの右or左にある
             analogWrite(5, 0);
             analogWrite(6, 0);
             analogWrite(7, 125);
             analogWrite(8, 0);
             analogWrite(9, 0);
             analogWrite(10, 150);
-          } else {
+          
+        }*/
+          else {
             analogWrite(5, 0);
             analogWrite(6, 0);
             analogWrite(7, 0);
@@ -113,14 +114,14 @@ void loop() {
           analogWrite(8, 0);
           analogWrite(9, 50);
           analogWrite(10, 0);
-        } else {
+        } /*else {
           analogWrite(5, 0);
           analogWrite(6, 0);
           analogWrite(7, 0);
           analogWrite(8, 0);
           analogWrite(9, 0);
           analogWrite(10, 0);
-        }
+        }*/
       }
     }  // これより下にはコードは記述しない(関数を除く)
   }
